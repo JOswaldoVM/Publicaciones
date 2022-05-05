815685c4aa6af2b77dd3f0404cede12e43f829ba
JOswaldoVM <99057440+JOswaldoVM@users.noreply.github.com>
2022-05-04 23:27:05 -0500

    Add files via upload

---
+++ src/principal/EditorialPincipal.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package principal;
+
+import vista.Inicio;
+
+/**
+ *
+ * @author Jose Oswaldo
+ */
+public class EditorialPincipal {
+    public static void main(String[] args) {
+        Inicio in = new Inicio();
+        in.setVisible(true);
+    }
+}
